f92ae23d99859389428aa5cecdd41ae764b620c9
Göktuğ Selçuk <42749496+goktugselcuk@users.noreply.github.com>
2024-06-13 11:50:45 +0300

    Second Phase
    ++Added Database for English words.
    
    ++Added WordCheck() function to check submitted words
    
    ++ Added Eraser powerup
    
    ++ Added functionality to calculate and display player score
    
    ++ Renewed UI Design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Purchasing;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
+using Image = UnityEngine.UI.Image;
+using Slider = UnityEngine.UI.Slider;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] GameObject tilePrefab;
     [SerializeField] TextMeshProUGUI wordText;
-    float leftBound = -8.16f;
-    float timeToStart = 1.0f;
-    float repeatRate = 1.0f;
+    [SerializeField] TextMeshProUGUI scoreText;
+    //[SerializeField] TextMeshProUGUI countDownText;
+
+    public GameObject titleScreen;
+    public GameObject playScreen;
+
+   
+
+    static float leftBound = -8.16f;
+    //static float timeToStart = 1.0f;
+    float spawnRate = 1.0f;
     private LetterTile tileScript;
     string word="";
+
+    Vector2 fixedPos1 = new Vector2(leftBound, 1.0f);
+    Vector2 fixedPos2 = new Vector2(leftBound, 0.0f);
+    Vector2 fixedPos3 = new Vector2(leftBound, -1.0f);
+
+    float roundTime = 60;
+    public Slider timerBar1;
+    public Slider timerBar2;
+    public Image gameOverScreen;
+    public bool gameOver=false;
+    Color redColor = Color.red;
+
+    public int score;
     
 
-    //float roundTime = 60;
+  
     // Start is called before the first frame update
     void Start()
     {
+        titleScreen.gameObject.SetActive(true);
+        timerBar1.maxValue = roundTime;
+        timerBar1.value = roundTime;
+        timerBar2.maxValue = roundTime;
+        timerBar2.value = roundTime;
         
-        UpdateWord("");
-        InvokeRepeating("Spawner", timeToStart, repeatRate);
+        
+        //InvokeRepeating("Spawner", timeToStart, repeatRate);
     }
 
     // Update is called once per frame
@@ -29,34 +65,140 @@ public class GameManager : MonoBehaviour
     {
        
     }
-
-    void Spawner()
+    void StartTimer()
     {
-        Vector2 fixedPos1 = new Vector2(leftBound, 1.0f);
-        Vector2 fixedPos2 = new Vector2(leftBound, 0.0f);
-        Vector2 fixedPos3 = new Vector2(leftBound, -1.0f);
-        Instantiate(tilePrefab, fixedPos1, tilePrefab.transform.rotation);
-        Instantiate(tilePrefab, fixedPos2, tilePrefab.transform.rotation);
-        Instantiate(tilePrefab, fixedPos3, tilePrefab.transform.rotation);
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown()
+    {
+
+        while (gameOver == false)
+        {
+            roundTime -= Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+
+            if (roundTime < 4)
+            {
+
+                timerBar1.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = redColor;
+                timerBar2.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = redColor;
+            }
+
+            if (roundTime <= 0)
+            {
+                gameOver = true;
+                GameOver();
+            }
+
+            if (gameOver == false)
+            {
+                timerBar1.value = roundTime;
+                timerBar2.value = roundTime;
+            }
+        }
         
     }
 
-    public void UpdateWord(string letter)
+    void GameOver()
+    {
+        gameOver = true;
+        Debug.Log("NAZ YENDI");
+        gameOverScreen.gameObject.SetActive(true);
+    }
+ 
+    public void AddLetter(string letter)
     {
         word += letter;
         wordText.text = word;
     }
 
+    public void EraseLetter()
+    {
+        Debug.Log("In EraseLetter()");
+        Debug.Log("word before remove: "+word);
+        //word.Remove(word.Length - 1, 1);
+        word = word.Substring(0, word.Length - 1);
+        Debug.Log("word after remove: "+word);
+        wordText.text = word;
+    }
+
+    public void StartGame()
+    {
+        gameOver = false;
+        AddLetter("");
+        titleScreen.gameObject.SetActive(false);
+        playScreen.gameObject.SetActive(true);
+        score = 0;
+
+        StartCoroutine("spawnTiles");
+        StartTimer();
+        scoreText.text = "" + score;
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+    IEnumerator spawnTiles()
+    {
+        while (!gameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            Instantiate(tilePrefab, fixedPos1, tilePrefab.transform.rotation);
+            Instantiate(tilePrefab, fixedPos2, tilePrefab.transform.rotation);
+            Instantiate(tilePrefab, fixedPos3, tilePrefab.transform.rotation);
+
+        }
+
+    }
+
+    public void SubmitWord()
+    {
+        
+        if (WordCheck(word))
+        {
+            Debug.Log("Word " +word+" exists.");
+            int wordLength = word.Length;
+            score += wordLength * 100;
+            scoreText.text = ""+score;
+        }
+        else 
+        {
+            Debug.Log("Word " + word + " does not exists.");
+        }
+        DeleteWord();
+    }
+
+    public void DeleteWord()
+    {
+        word = "";
+        wordText.text = word;
+    }
+
+    bool WordCheck(string word)
+    {
+        Debug.Log("----------------------------------------------");
+        Debug.Log("WordCheck starts..");
+        using (var sr = new StreamReader("C:\\Users\\goktu\\LetterFallGame\\Assets\\Databases\\words_alpha.txt", true))
+            for (string line; (line = sr.ReadLine()) != null;) //read line by line
+                if (line.Equals(word.ToLower()))
+                {
+                    Debug.Log("checked line is: " + line + " for the target word: " + word.ToLower());
+                    Debug.Log("wordcheck success.. returning true");
+                    return true;
+                }
+        Debug.Log("wordcheck failure.. returning false");
+        Debug.Log("----------------------------------------------");
+        return false;
+    }
+
     
 
-    /*
-    void Countdown()
-    {
-        if (roundTime > 0)
-        {
-            roundTime -= Time.deltaTime;
-        }
-        //countDownText.text=""
-    }
-    */
+    
+   
+    
 }
--- Assets/Scripts/LetterTile.cs
+++ Assets/Scripts/LetterTile.cs
@@ -1,34 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class LetterTile : MonoBehaviour
 {
     [SerializeField] float speed;
     TMP_Text textObject;
+    //GameObject imageObject;
+    //SpriteRenderer imageSR;
+    //public Sprite eraserIcon;
     int[] choiceSelection;
     //bool hasLetter = false;
     int letterLottery;
+    int powerUpLottery;
     string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+    string[] powerups = { "Eraser" };
     int letterChoice;
-    public string letter="";
+    int powerUpChoice;
+    public string letter = "";
     public TMP_Text wordText;
     GameManager gameManagerScript;
-    
+    public int mghee;
+    Transform eraserIcon;
+
     // Start is called before the first frame update
     void Start()
     {
+        eraserIcon=transform.Find("Eraser");
         gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
         textObject = GetComponentInChildren<TMP_Text>();
+        //imageObject = GameObject.Find("/Image/EraserSprite");
+         
+        
+        
+       
 
-        letterLottery = Random.Range(1, 4);
+        letterLottery = Random.Range(1, 3);
+        powerUpLottery = Random.Range(1, 3);
+        
         if (letterLottery == 1)
         {
             letterChoice = Random.Range(0, letters.Length);
             textObject.text = letters[letterChoice];
+
+        }
+        else
+        {
+            
+            eraserIcon.gameObject.SetActive(true);
+            
+            //powerUpChoice = Random.Range(0, powerups.Length);
+            //imageSR.sprite = eraserIcon;
 
         }
     }
@@ -36,18 +64,32 @@ public class LetterTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+
         if (transform.position.x > 10.0)
         {
             Destroy(gameObject);
         }
-    }
 
-    void OnMouseDown()
-    {
-        letter=textObject.text; 
-        gameManagerScript.UpdateWord(letter);   
-        textObject.text = " ";
-        
     }
+    void OnMouseDown()
+     {
+
+        if (letterLottery == 1)
+        {
+            letter = textObject.text;
+            gameManagerScript.AddLetter(letter);
+            textObject.text = " ";
+        }
+
+        else
+        {
+            Debug.Log("Eraser clicked!");
+            gameManagerScript.EraseLetter();
+        }
+
+    }
+    
 }
